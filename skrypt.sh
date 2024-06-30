5aa9d3a3fece75e97c66e7e0b4e419bd5d1b0701
strangeyears0 <strangeyears0@gmail.com>
2024-06-30 18:30:50 +0200

    Add --logs flag to skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 case "$1" in
---date)
-date
-;;
-#)
-echo "Unknow option : $1"
-;;
+  --date)
+    date
+    ;;
+  --logs)
+    if [ -z "$2" ]; then
+      num=100
+    else
+      num=$2
+    fi
+    for ((i=1; i<=num; i++)); do
+      echo "File number $i created by skrypt.sh on $(date)" > "log_$i.txt"
+    done
+    ;;
+  *)
+    echo "Unknown option: $1"
+    ;;
 esac
+
